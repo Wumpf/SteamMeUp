22fe3bf108abf841b961479ac9b67040aabb88e4
Andreas Reich <r_andreas2@web.de>
2018-12-03 00:22:31 +0100

    Basic working prototype

--- SteamMeUp.cpp
+++ SteamMeUp.cpp
@@ -1,21 +1,122 @@
-// SteamMeUp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <windows.h>
+#include <Xinput.h>
+#include <cstdio>
+#include <string>
 
-#include "pch.h"
-#include <iostream>
+// Undocumented button constants
+#define XINPUT_GAMEPAD_GUIDE_BUTTON 0x0400
+#define XINPUT_GAMEPAD_UNKNOWN		0x0800
+
+typedef struct
+{
+	unsigned long eventCount;
+	WORD          wButtons;
+	BYTE          bLeftTrigger;
+	BYTE          bRightTrigger;
+	SHORT         sThumbLX;
+	SHORT         sThumbLY;
+	SHORT         sThumbRX;
+	SHORT         sThumbRY;
+} XINPUT_STATE_EX;
+int(__stdcall *XInputGetStateEx) (int, XINPUT_STATE_EX*);
+
+wchar_t steamInstallationPath[MAX_PATH];
+
+bool Startup()
+{
+	DWORD bufferSize = sizeof(steamInstallationPath);
+	if (RegGetValue(HKEY_CURRENT_USER,
+					L"Software\\Valve\\Steam",
+					L"SteamExe",
+					RRF_RT_REG_SZ,
+					NULL,
+					steamInstallationPath,
+					&bufferSize) == ERROR_SUCCESS)
+	{
+		wprintf(L"Found steam installation at: %s\n", steamInstallationPath);
+	}
+	else
+	{
+		printf("Failed to find steam installation!\n");
+		return false;
+	}
+
+	// Query secret gamepad query function that allows to get the guide button.
+	// The default XInputGetState will never tell us about the guide button.
+	// https://forums.tigsource.com/index.php?topic=26792.msg847843#msg847843
+	wchar_t xinputDllPath[MAX_PATH];
+	GetSystemDirectory(xinputDllPath, sizeof(xinputDllPath));
+	wcscat_s(xinputDllPath, L"\\xinput1_4.dll");
+	HINSTANCE xinputDll = LoadLibrary(xinputDllPath);
+	if (!xinputDll)
+	{
+		wprintf(L"Failed to load xinput dll at: %s\n", xinputDllPath);
+		return false;
+	}
+	XInputGetStateEx = (decltype(XInputGetStateEx))GetProcAddress(xinputDll, (LPCSTR)100);
+
+
+	// Deprecated?
+	//XInputEnable(TRUE);
+
+	return true;
+}
+
+void StartSteamInBigPictureMode()
+{
+	wchar_t steamBigPictureStartupCommandLine[MAX_PATH * 2];
+	wsprintf(steamBigPictureStartupCommandLine, L"%s -bigpicture", steamInstallationPath);
+
+	wprintf(L"Launching steam in big picture mode via \"%s\"...\n", steamBigPictureStartupCommandLine);
+
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+	if (CreateProcess(NULL, steamBigPictureStartupCommandLine, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi))
+		printf("Started steam successfully.\n");
+	else
+		printf("Failed to start steam: %i\n", GetLastError());
+}
+
+void RunLoop()
+{
+	const int updateFrequencyHz = 10;
+
+	XINPUT_STATE_EX oldState[XUSER_MAX_COUNT];
+	ZeroMemory(oldState, sizeof(oldState));
+
+	while (true)
+	{
+		for (int i = 0; i < XUSER_MAX_COUNT; ++i)
+		{
+			XINPUT_STATE_EX newState;
+			auto result = XInputGetStateEx(i, &newState);
+			if (result)
+			{
+				ZeroMemory(&oldState[i], sizeof(oldState[i]));
+				continue;
+			}
+
+			// Released guide button - this way turning off doesn't trigger this.
+			if ((oldState[i].wButtons & XINPUT_GAMEPAD_GUIDE_BUTTON) != 0 && 
+				(newState.wButtons    & XINPUT_GAMEPAD_GUIDE_BUTTON) == 0)
+			{
+				StartSteamInBigPictureMode();
+			}
+
+			oldState[i] = newState;
+		}
+		Sleep(1000 / updateFrequencyHz);
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	if (Startup())
+		RunLoop();
+	else
+		return 1;
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
